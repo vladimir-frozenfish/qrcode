7393a5180efc73a2910e1f827b3a5e58717ab447
Vladimir_Kirichenko <frozenfish@internet.ru>
2022-06-09 22:30:33 +0300

    qrcode_2_generate.py - добавлено сохранение файла по умолчанию на основе текущей даты и времени

--- qrcode_2_generate.py
+++ qrcode_2_generate.py
@@ -6,6 +6,7 @@
 """
 
 
+import time
 import sys
 import pyqrcode
 
@@ -22,6 +23,15 @@ except UnicodeEncodeError as error:
     print(error)
     sys.exit()
 
-qr_file_name = input('Введите имя файла для QRcode: ')
+qr_file_name = input('Введите имя файла для QRcode (enter - по умолчанию): ')
+
+"""если строка имени файла пустая, то имя файла формируется 
+на основе текущей даты и времени"""
+if not qr_file_name:
+    time_file = time.localtime()
+    qr_file_name = (f'qrcode_'
+                    f'{time_file.tm_year}{time_file.tm_mon}{time_file.tm_mday}_'
+                    f'{time_file.tm_hour}{time_file.tm_min}{time_file.tm_sec}')
+
 qr.png(f'img/{qr_file_name}.png', scale=10)
 
