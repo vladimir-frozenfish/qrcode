bfa36002f46cb24965fc5a74bcc82c842dde8ee5
Vladimir_Kirichenko <frozenfish@internet.ru>
2022-06-11 09:33:03 +0300

    qrcode_1.py - добавлены разные способы сохранения qrcode

--- qrcode_1.py
+++ qrcode_1.py
@@ -1,4 +1,5 @@
 """генерация и расшифровка QRcode"""
+import io
 
 import pyqrcode
 from pyzbar.pyzbar import decode
@@ -7,10 +8,25 @@ from PIL import Image
 # генерация QRcode
 qr_str = 'Hallo! My name is Vladimir Kirichenko!'
 qr = pyqrcode.create(qr_str)
-qr.png('img/qrcode.png', scale=10)
+
+qrcode_text = qr.terminal(quiet_zone=1) # сохранение qrcode в текстовом формате
+print(qrcode_text)
+
+qr.png('img/qrcode.png', scale=10)      # сохранение qrcode в файл
+
+buffer = io.BytesIO()
+qr.png(buffer)                          # сохранение qrcode в буфер
+print(list(buffer.getvalue()))
+
+qrcode_as_str = qr.png_as_base64_str(scale=5)
+html_img = '<img src="data:image/png;base64,{}">'.format(qrcode_as_str)     # сохранение qrcode для html
+print(qrcode_as_str)
+print(html_img)
+
 
 # чтение QRcode
-qr_decode = decode(Image.open('img/qrcode.png'))
+qr_image = Image.open('img/qrcode.png')
+qr_decode = decode(qr_image)
 print(qr_decode[0].data.decode('ascii'))
 
 
