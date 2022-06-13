cbe524373b1245ce89aa5c0c011e5fe33b5d355b
Vladimir_Kirichenko <frozenfish@internet.ru>
2022-06-13 22:56:20 +0300

    qrcode2_decode.py - сделана обработка ошибки загрузки файла не являющегося изображением - если qrcode в изображении не обнаружен или не распознан выводится соответствующее изображение

--- qrcode2_decode.py
+++ qrcode2_decode.py
@@ -1,9 +1,16 @@
 from pyzbar.pyzbar import decode
-from PIL import Image
+from PIL import Image, UnidentifiedImageError
 
 # чтение QRcode
-qr_image = Image.open('img/qr4.jpg')
-qr_decode = decode(qr_image)
-print(f'{qr_image= }')
-print(f'{qr_decode= }')
-print(qr_decode[0].data.decode('utf-8'))
+try:
+    qr_image = Image.open('img/qr2.jpg')
+    qr_decode = decode(qr_image)
+    print(f'{qr_image= }')
+    print(f'{qr_decode= }')
+    if qr_decode:
+        print(qr_decode[0].data.decode('utf-8'))
+    else:
+        print('Не удалось распознать в изображении QRcode!')
+except UnidentifiedImageError as error:
+    print('Загруженный файл не является изображением!')
+    print(error)
